bbce69dcfb19c579a6b082236afd7bb5a2afe18d
Matthias Kretschmann <krema@jpberlin.de>
2011-06-01 19:46:24 +0200

    simplified loop stuff, no more parts; theme meta data; simplified css files

--- page.php
+++ page.php
@@ -14,13 +14,21 @@
 
 get_header(); ?>
 
-			<?php
-			/* Run the loop to output the page.
-			 * If you want to overload this in a child theme then include a file
-			 * called loop-page.php and that will be used instead.
-			 */
-			get_template_part( 'loop', 'page' );
-			?>
+	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+	
+			<?php if ( is_front_page() ) { ?>
+				<h2><?php the_title(); ?></h2>
+			<?php } else { ?>
+				<h1><?php the_title(); ?></h1>
+			<?php } ?>
+
+					<?php the_content(); ?>
+					<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
+					<?php edit_post_link( __( 'Edit', 'twentyten' ), '|', '' ); ?>
+			
+			<?php comments_template( '', true ); ?>
+	
+	<?php endwhile; ?>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
