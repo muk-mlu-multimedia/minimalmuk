ece83d7e08a2a7766e47586c793fc8267a2e6cbd
Matthias Kretschmann <krema@jpberlin.de>
2011-06-09 16:54:43 +0200

    Footer links to github & creators

--- footer.php
+++ footer.php
@@ -6,8 +6,11 @@
 	
 		<footer id="unten">
 			<p><small>
-				<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+				<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> | 
 				<a href="http://wordpress.org/" title="Semantic Personal Publishing Platform" rel="generator">Proudly powered by WordPress </a>
+			</small></p>
+			<p><small>
+				<a href="https://github.com/kremalicious/minimalmuk">minimalMuk Theme</a> by <a href="http://matthiaskretschmann.com/">Matthias Kretschmann</a> &amp; <a href="http://www.anketornow.de/">Anke Tornow</a>
 			</small></p>
 		</footer>
 	
